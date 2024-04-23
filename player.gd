3a91f449263fe5ac4b9b148185fbf1184a864b98
platz-halter <simon.pressel2@proton.me>
2024-04-23 22:41:41 +0200

    Initialised repo

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var max_speed = 50
+@export var jump_power = 50
+
+func _ready():
+	print("Hello world")
+func _physics_process(delta):
+	velocity.y += 2
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = max_speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -max_speed
+	else:
+		velocity.x = 0	
+	
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -jump_power
+	
+	
+	move_and_slide()
+	
+
